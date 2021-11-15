7ad62f7b8d4a616acad9aa315ca1efe27beeaafb
TheLe0 <leob.tosin@hotmail.com>
2021-11-15 10:52:33 -0300

    Create server class

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,4 @@
-import express from 'express';
-import { makeLogger } from './logger';
+import { Server } from './server';
 
-const app = express();
-const logger = makeLogger();
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Made with 💙 by TheLe0');
-});
-
-app.listen(port, () => {
-  logger.debug({
-    type: 'LOG_TYPE_1',
-    message: `Server listening on port ${port}`
-  });
-
-  console.log(`Server running on http://localhost:${port}`)
-})
+const server = new Server();
+server.start();
