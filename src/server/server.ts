5f9c190b9089ba6656604508093194a389e69c1a
TheLe0 <leob.tosin@hotmail.com>
2021-11-24 21:26:40 -0300

    Add .ENV and configurations

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { makeLogger } from '../log';
 import routes  from './routes';
+import { serverConfig } from '../config/server';
 
 export default class Server {
     private readonly app: any;
@@ -17,7 +18,7 @@ export default class Server {
 
     public start() {
         this.setupRoutes();
-        this.app.listen(3000, () => {
+        this.app.listen(serverConfig.Port, () => {
             this.log.debug({
                 type: 'LOG_TYPE_1',
                 message: `Server listening on port 3000`
